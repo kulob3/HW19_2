a7cfa1d2ed71c161d5f656dfb7e8ab5e9679a2e1
Alex Urapin <kulob1@mail.ru>
2024-09-09 13:00:22 +0400

    add admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from catalog.models import Product, Category
 
-# admin.site.register(Student)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
 
 
 
