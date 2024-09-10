6e43b17787f907e3b0d4e8da030e080203faa690
Alex Urapin <kulob1@mail.ru>
2024-09-10 11:50:41 +0400

    change

--- HW19_2/settings.py
+++ HW19_2/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'catalog',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'lenovo201',
         'HOST': 'localhost',
         'PORT': '5432',
     }
